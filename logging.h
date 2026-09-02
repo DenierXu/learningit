32237465d00eb2d90c853f8160872f6ed6ddf071
Jinshan XU <jxu@zjut.edu.cn>
2026-09-02 14:54:41 +0800

    save logging.h

---
+++ logging.h
@@ -0,0 +1,18 @@
+/*
+ *  logging.h
+ *  
+ *
+ * logging and debug functions
+ *
+ */
+
+
+#define LOG_IN_FILE  0x0001
+#define LOG_SCREEN   0x0010
+
+#define logfilename "log.txt"
+
+extern int LOG_WHAT; 
+
+/* write a message into the log file or print on screen */
+int debug(const char *fmt, ...); 
